859b7cca7491f76fb3585b3e1e9c36219bcb544f
yeojoey <yeojoey@gmail.com>
2015-06-03 09:34:51 +0800

    FUCK

--- aroundNUS.js
+++ aroundNUS.js
@@ -3,15 +3,15 @@ var jf = require("jsonfile");
 var stationsFilepath =  './stations.json';
 var stations = jf.readFileSync(stationsFilepath);
 var stationsArray; var passwordsArray;
-//var lettersArray;
+var lettersArray;
 readStations(stations);
-
+console.log(lettersArray);
 var wrongPasswordMsg = "Wrong password!";
 
 function getTask(password) {
 	for(var i=0; i<passwordsArray.length; i++) {
         if (password == passwordsArray[i]) {
-            return "TASK: " + stationsArray[i].task + "\nAVAILABLE LETTERS: " ;
+            return "TASK: " + stationsArray[i].task + "\nAVAILABLE LETTERS: " + lettersArray[i];
         }
     }
     return wrongPasswordMsg;
@@ -20,21 +20,29 @@ function getTask(password) {
 function readStations(station_list) {
     var arr_stations = [];
     var arr_passwords = [];
+    var arr_letters = [];
     for (var station_name in station_list) {
         if (station_list.hasOwnProperty(station_name)) {
             var station = station_list[station_name];
             arr_stations.push(station);
-            for (var password in station) {
-                if( password == "password") {
-                    if (station.hasOwnProperty(password)){
-                        arr_passwords.push(station[password]);
+            for (var property in station) {
+                if( property == "password") {
+                    if (station.hasOwnProperty(property)){
+                        arr_passwords.push(station[property]);
                     } 
                 }
+                if (property == "letters") {
+                    if (station.hasOwnProperty(property)){
+                        arr_letters.push(station[property]);
+                    }
+                }
             }
+            
         }
     }
     stationsArray = arr_stations;
     passwordsArray = arr_passwords;
+    lettersArray = arr_letters;
 }
 
 
